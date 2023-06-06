25a4e2803461ca2337bc260a277c8d1b864dcfde
BAH Mamadou <bah311802@gmail.com>
2023-06-06 21:44:44 +0200

    Dossier script recuperation d'element du DOM dans le main.js

---
+++ mes_scripts/main.js
@@ -0,0 +1,14 @@
+//Selection d'un element du DOME avec la méthode getElementById
+
+let inputTexte = document.getElementById("inputEcriture");
+let buttonValide = document.getElementById("btnValiderMot");
+
+//Selection d'un element du DOME avec la méthode querySelector
+
+let zoneMotPropose=document.querySelector(".zoneProposition");
+let zoneScore=document.querySelector(".zoneScore span");
+
+//Selection d'un element du DOME avec la méthode querySelectorAll
+
+let allButtonRadio = document.querySelectorAll(".optionSource input");
+
